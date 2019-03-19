aae86f7c1a038a3707dd63196287acbbafa9dd4e
theonak4 <38365756+theonak4@users.noreply.github.com>
2019-03-18 17:27:21 -0700

    Finished

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
-
+//
 public class FileDecryptor {
 	// Create a program that opens the file created by FileEncryptor and display
 	// the decrypted message to the user in a JOptionPane.
