1ca6561c8a06c092c4f8d1a3806a974484741295
theonak4 <38365756+theonak4@users.noreply.github.com>
2019-03-18 17:26:57 -0700

    Finished File Recorder

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
-
+//
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
 	public static void main(String[] args) {
